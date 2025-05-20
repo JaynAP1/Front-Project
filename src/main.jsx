311c52b10bf5601651a591c970b8af6511316347
Luis <roronoazoro3704@gmail.com>
2025-05-19 20:54:22 -0500

    feat: :sparkles: enpoints of reserves

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Reservas from './Modules/Reservas/Reservas.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Reservas />
   </StrictMode>,
 )
